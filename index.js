e6122318fdfa90e014b0a34722c63003dc28677c
SundarJ <sundarjoshi@hotmail.co.uk>
2015-01-28 19:06:09 +0000

    delete node_modules

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
+var system = require('child_process').exec;
+
+app.get('/', function(req, res, next) {
+   console.log(req.headers["accept-language"]);
+   next();
+});
 
 app.use(compression());
 app.use(express.static(__dirname));
 app.listen(8080);
-console.log("server started");
+system("google-chrome 0.0.0.0:8080", function (e, out, err) {});
